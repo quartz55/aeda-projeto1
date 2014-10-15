c970b3aa824f671f60f483045e1654133dbaf7f8
quartz55 <john.razor97@gmail.com>
2014-10-15 11:42:22 +0100

    Modified jogadores to socios

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,15 @@ int main(){
 
     Clube clube;
 
-    //Pessoa *p1 = new Pessoa("Joao Costa", 18, 0);
-    //p1->setExterno();
-
-    Jogador *p1 = new Jogador("Joao Costa", 18, 14702805);
+    Socio *p1 = new Socio("Joao Costa", 18, 14702805, "Macho");
     clube.addJogador(p1);
-    Jogador *p2 = new Jogador("Joao Almeida", 19, 123123);
+    Socio *p2 = new Socio("Joao Almeida", 19, 123123, "Macho");
     clube.addJogador(p2);
-    p2 = new Jogador("Hugo Almeida", 25, 592312);
+    p2 = new Socio("Hugo Almeida", 25, 592312, "Macho");
     clube.addJogador(p2);
-    p2 = new Jogador("Cardoso", 24, 292312);
+    p2 = new Socio("Cardoso", 24, 292312, "Femea");
     clube.addJogador(p2);
-    p2 = new Jogador("Luisao", 94, 219234);
+    p2 = new Socio("Luisao", 94, 219234, "Femea");
     clube.addJogador(p2);
 
     Modalidade *m1 = new Modalidade("Futebol");
