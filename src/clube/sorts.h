b138260ec74909ef6383cf5ec65fa588fd5893d4
quartz55 <john.razor97@gmail.com>
2014-11-03 15:37:51 +0000

    Added sorts.h for sorting functions

---
+++ src/clube/sorts.h
@@ -0,0 +1,2 @@
+bool sortByName(const Pessoa *lhs, const Pessoa *rhs) { return lhs->getNome() < rhs->getNome();}
+bool sortByIdade(const Pessoa *lhs, const Pessoa *rhs) { return lhs->getIdade() < rhs->getIdade();}
